009f92c7f3d075b411f755a670df6956c9d07e54
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-06-03 15:52:30 -0500

    Adding statement and function JS excercises

---
+++ public/js/user-interaction.js
@@ -0,0 +1,29 @@
+"use strict";
+	
+	// TODO: Ask the user for their name.
+	
+
+
+			 var name= prompt('What is your name?');
+            	while (name == "" || name == "null") {
+                name = prompt('Please tell me your name.')
+            }
+            	alert("Hey there, " + name);
+	
+			var pizza = confirm("Do you like pizza, " + name + "?");
+				if (pizza){
+					alert ("Awesome! I like pizza too.");
+
+				} else {
+					alert("But I thought everyone likes pizza? I bet you don't like puppies either you monster.");
+				}
+	//       Keep asking if an empty input is provided.
+	
+	// TODO: Show an alert message that welcomes the user based on their input.
+	
+	// TODO: Ask the user if they like pizza.
+	//       Based on their answer show a creative alert message.
+	
+	
+	
+	
