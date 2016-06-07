f3026d4d652ee2ef3592e4114fc08af77bfc0c9d
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-06-07 09:10:19 -0500

    shortened code

--- public/js/circle.js
+++ public/js/circle.js
@@ -17,7 +17,8 @@
             // TODO: complete this method.
             var area = this.getArea();
                 if (doRounding) {
-                    console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(area));
+                    area = Math.round(area);
+                    // console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(area));
                 } else {
                     console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
                 }
@@ -29,16 +30,6 @@
         }
     };
 
-     // log info about the circle
-    console.log("Raw circle information");
-    circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
-
-    console.log("=======================================================");
-
-
-    circle.radius= 5;
          // log info about the circle
         console.log("Raw circle information");
         circle.logInfo(false);
@@ -46,7 +37,17 @@
         circle.logInfo(true);
     
         console.log("=======================================================");
-
+    
+    
+        circle.radius= 5;
+             // log info about the circle
+            console.log("Raw circle information");
+            circle.logInfo(false);
+            console.log("Circle information rounded to the nearest whole number");
+            circle.logInfo(true);
+        
+            console.log("=======================================================");
+    
 
 
 })();
