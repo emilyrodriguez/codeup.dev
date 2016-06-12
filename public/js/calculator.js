02d53a68ffc87b4793dc08561adfa9b1b4065321
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-06-12 15:16:08 -0500

    Made changes to JS and HTML. Still a work in progress.

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,28 +1,116 @@
 "use strict";
+(function() {
+// left side input
+	function clearDisplay() {
+	    var display = document.getElementById('display');
+	    	display.value = '0';
+	    	storedNum = '0';
+	    	calcFinished = true;
+	    	operation = operations.none;
+	}
+	
+	function clearPrevious() {
+	    var display = document.getElementById('display');
+	    if (calcFinished) {
+	        display.value = '0';
+	        calcFinished = false;
+	    }
+	}
+	
+	function numInput(digits) {
+	    var display = document.getElementById('leftSide');
+	    	clearPrevious();
+	    if (display.value === '0') display.value = '';
+	    	display.value += digit;
+	}
+	
+	function insertDecimal() {
+	    var display = document.getElementById('leftSide');
+	    	clearPrevious();
+	    if (display.value.indexOf('.') === -1) display.value += '.';
+	}
+	
+// math operations
 
-var storeNumber = function(event) {
-    if (operator != "") {
-        var numberInput = document.getElementById("firstNumber");
-        numberInput.value += this.getAttribute('data-value');
-    } else { 
-        var numberInput = document.getElementsById("secondNumber");
-        numberInput.value = this.getAttribute('data-value');
-    }
-}
-var storeSecondInput = function(event) {
-    var addOperator = document.getElementById("operator");
-    addOperator.value += this.getAttribute('data-value');
-}
+	function add() {
+		var display = document.getElementById('operator');
+		setOperation
+	}
+	function subtract() {
+		var display = document.getElementById('operator');
+	}
+
+	function multiply() {
+		var display = document.getElementById('operator');
+	}
+	function divide() {
+		var display = document.getElementById('operator');
+	}
 
 
-//events
+
+// right side output
+	function setOperation(command) {
+	    var display = document.getElementById('rightSide');
+	    	calculate();
+	    	storedNum = display.value;
+	    if (operations.hasOwnProperty(command))
+	        operation = operations[command];
+	}
+	
+	function calculate() {
+	    var display = document.getElementById('rightSide');
+	    	display.value = operation(+ storedNum, + display.value);
+	    	calcFinished = true;
+	    	operation = operations.none;
+	}
+	
+// events
+	if ('addEventListener')
+	    window.addEventListener('load', clearDisplay);
+	else
+	    window.attachEvent('onload', clearDisplay);
+	}());
+	
 
 
-var numberInput = document.getElementsByClassName("numberButton");
-    for(var i = 0; i < numberInput.length; i++) {
-        numberInput[i].addEventListener('click', storeNumber , false);
-    }
-var operatorInput = document.getElementsByClassName("operatorButtons");
-    for(var i = 0; i < operatorInput.length; i++) {
-        operatorInput[i].addEventListener('click', storeSecondInput , false);
-    }
+
+
+
+// 	 var storeNumber = function(event) {
+// 		 var operators = document.getElementById("signs");
+// 		 if (operators !== "signs") {
+// 			 var numberInput = document.getElementById("leftSide");
+// 			 numberInput.value += this.getAttribute('data-value');
+// 		 } else {
+// 			 var numberInput = document.getElementById("signs");
+// 			 numberInput.value = this.getElementById('output-area-center');
+// 		 }
+			
+// 	 }
+	
+
+// //events
+
+
+// 	var numberInput = document.getElementsByClassName("digits");
+// 	    for(var i = 0; i < numberInput.length; i++) {
+// 	        numberInput[i].addEventListener('click', storeNumber);
+// 	    }
+// 	    console.log(numberInput);
+
+// 	var operatorInput = document.getElementById("signs");
+// 	    for(var i = 0; i < operatorInput.length; i++) {
+// 	        operatorInput[i].addEventListener('click', storeSecondInput);
+// 	    }
+// 	    console.log(operatorInput);
+
+// 	var printLeft= document.getElementsByClassName("digits");
+// 	    document.getElementsByClassName("digits").addEventListener('click', printLeft);
+// 	    printLeft();
+
+
+
+
+
+
