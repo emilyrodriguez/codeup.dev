d0296ba1cf8f58d2b05ad6087f60652eea89984b
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-13 16:03:21 -0500

    generates randomized nouns/adjs to create server names

---
+++ public/server-name-generator.php
@@ -0,0 +1,17 @@
+<?php
+	$nouns = ['art', 'music', 'glitter', 'dragons', 'kazoos', 'mermaids', 'unicorns', 'rainbows', 'puppies', 'lasers'];
+	$adjectives = ['grumpy', 'dangerous', 'miniature', 'mysterious', 'obnoxious', 'slippery', 'colassal', 'fluffy', 'magnificent', 'scattered'];
+	$randomizer = $adjectives[mt_rand(0, count($adjectives) -1)]. " " . $nouns[mt_rand(0, count($nouns) -1)];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Server Name Generator</title>
+</head>
+<body>
+	<div id="generator">
+		<h1>Do you like... </h1>
+			<?php echo $randomizer . "?\n"?>
+	</div>
+</body>
+</html>
