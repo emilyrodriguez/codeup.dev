d55bcc7ffb81a0eaa81e85f6c844619b5f5b91e1
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-08-05 14:43:46 -0500

    changed visability

--- public/log.php
+++ public/log.php
@@ -1,19 +1,30 @@
 <?php
-class Log {
-	public $filename;
+class Log{
+	private $handle;
+	// private $filename; 
+
+	public function __construct($prefix = "log-") {
+		$this->filename = "$prefix" . date("YYYY-mm-dd") . ".log";
+		$this->handle = fopen($this->filename((string)$prefix), 'a');
+	}
+	private function filename($prefix) {
+		$filename = "$prefix-" . date('Y-m-d') . '.log';
+		if (!touch($filename) || !is_writable($filename)) {
+			echo '$filename cannot be modified!';
+		}
+	}
 	public function logMessage($level, $message) {
-		$handle = fopen($this->filename, "a");
-		$formattedMessage = date("Y-m-d H:i:s") . " [$level] $message";
-		fwrite($handle, $formattedMessage);
-		fclose($handle);
+		$formatMessage = date("Y-m-d H:i:s") . " [$level] $message";
+		fwrite($handle, $formatMessage);
+	}
+	public function __destruct() {
+		fclose($this->handle);
 	}
 	public function info($message) {
-		$this->logMessage("INFO", $message);
+		$this->logMessage("User $username logged in", $message);
 	}
 	public function error($message) {
-		$this->logMessage("ERROR", $message);
+		$this->logMessage("User $username failed to login", $message);
 	}
 }
 ?>
-
- 
