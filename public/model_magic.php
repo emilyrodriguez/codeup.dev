37dab57b201509672bd8c20f3401d23142b76de3
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-08-05 14:45:16 -0500

    created class with private variable and used magic methods to access the data

---
+++ public/model_magic.php
@@ -0,0 +1,19 @@
+<?php
+class Model {
+	private $attributes = [];
+
+	public function __set($key, $value) {
+		$this->attributes[$key] = $value;
+
+	}
+	public function __get($key) {
+		if (array_key_exists($key, $this->attributes)) {
+			return $this->attributes[$key];
+		}
+		return null;
+	}
+}
+
+	$model = new Model();
+	$key->value = 'Emily';
+	var_dump($key);
